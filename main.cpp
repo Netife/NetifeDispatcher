58df933eeaf49021eed6ac5fb7f1b07692859f4d
EpicMo <1982742309@qq.com>
2023-04-30 19:55:42 +0800

    Probe improve

--- main.cpp
+++ main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <string>
+#include <grpcpp/grpcpp.h>
+#include "gRpcModel/NetifeMessage.pb.h"
+// 如果是想要调试本程序，请将本值设置为 True
+#define DEBUG_MODE true
 
-int main() {
-    std::cout << "[Netife Dispatcher] Netife is running... ..." << std::endl;
-    std::cout << "[Netife Dispatcher] Netife Dispatch is not a individual component, which need "
-                 "not only NetifeProbe but also NetifeCore." << std::endl;
+using namespace std;
+int main(int argc, char* argv[]) {
+
+    GOOGLE_PROTOBUF_VERIFY_VERSION; //自检版本
+
+    cout << "[Netife Dispatcher] Netife is running... ..." << endl;
+
+    // 参数自检
+    if (argc != 3 && !DEBUG_MODE){
+        cout << "[Netife Dispatcher] Netife Dispatch is not a individual component, which need "
+                "not only NetifeProbe but also NetifeCore." << endl;
+        return -1;
+    }
+
+    string hostIp;
+    int hostPort;
+
+    if (DEBUG_MODE){
+        hostIp = "127.0.0.1"; //调试模式预设默认调试地址
+        hostPort = 7899; //调试模式预设默认端口
+    }else{
+        hostIp = argv[1];
+        hostPort = stoi(argv[2]);
+    }
+
+    cout << "[Netife Dispatcher] The dispatcher service will run on the " << hostIp << ":" << hostPort << endl;
 
     return 0;
 }
