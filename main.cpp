4f4c03131ece9861e3686865d83fb9cbb812e386
EpicMo <1982742309@qq.com>
2023-04-30 11:06:08 +0800

    Probe gRpc Model

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "[Netife Dispatcher] Netife is running... ..." << std::endl;
+    std::cout << "[Netife Dispatcher] Netife Dispatch is not a individual component, which need "
+                 "not only NetifeProbe but also NetifeCore." << std::endl;
+
+    return 0;
+}
