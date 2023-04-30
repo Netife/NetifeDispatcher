dbc7ecef993e51bc1d6156325d7ca16445c25718
EpicMo <1982742309@qq.com>
2023-04-30 21:34:36 +0800

    proto class

---
+++ gRpcServices/NetifeServiceImpl.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2023/4/30.
+//
+
+#include "NetifeServiceImpl.h"
+
+namespace Netife {
+} // Netife
---
+++ gRpcServices/NetifeServiceImpl.h
@@ -0,0 +1,16 @@
+//
+// Created by Administrator on 2023/4/30.
+//
+
+#ifndef NETIFEDISPATCHER_NETIFESERVICEIMPL_H
+#define NETIFEDISPATCHER_NETIFESERVICEIMPL_H
+
+namespace Netife {
+
+class NetifeServiceImpl {
+
+};
+
+} // Netife
+
+#endif //NETIFEDISPATCHER_NETIFESERVICEIMPL_H
